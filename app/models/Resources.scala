b6b90afd5a7ea271fa32bc80d421838a52cf83f7
Umut Benzer <ub@ubenzer.com>
2013-05-08 15:51:41 +0300

    added salat plugin for mongodb access

---
+++ app/models/Resources.scala
@@ -0,0 +1,16 @@
+package models
+
+import play.api.Play.current
+import com.novus.salat.dao._
+import com.mongodb.casbah.Imports._
+import se.radley.plugin.salat._
+import com.novus.salat.global._
+
+case class Resources (
+  id: ObjectId = new ObjectId,
+  name: String
+)
+
+object Resources extends ModelCompanion[Resources, ObjectId] {
+  val dao = new SalatDAO[Resources, ObjectId](collection = mongoCollection("resources")) {}
+}
--- project/Build.scala
+++ project/Build.scala
@@ -4,18 +4,20 @@ import play.Project._
 
 object ApplicationBuild extends Build {
 
+  resolvers += Resolver.sonatypeRepo("snapshots")
+
   val appName         = "tezapp"
   val appVersion      = "1.0-SNAPSHOT"
 
   val appDependencies = Seq(
     // Add your project dependencies here,
     javaCore,
-    javaJdbc,
-    javaEbean,
     "commons-io" % "commons-io" % "2.4",
     "commons-validator" % "commons-validator" % "1.4.0",
     "xerces" % "xercesImpl" % "2.11.0",
-    "org.apache.jena" % "jena-core" % "2.10.0" excludeAll(ExclusionRule(organization = "org.slf4j"), ExclusionRule(organization = "xerces"))
+    "org.apache.jena" % "jena-core" % "2.10.0" excludeAll(ExclusionRule(organization = "org.slf4j"), ExclusionRule(organization = "xerces")),
+    "se.radley" % "play-plugins-salat_2.10" % "1.2",
+    "org.openrdf.sesame" % "sesame-rio" % "2.7.0"
   )
 
   // Only compile the bootstrap bootstrap.less file and any other *.less file in the stylesheets directory 
@@ -26,7 +28,11 @@ object ApplicationBuild extends Build {
   )
   
   val main = play.Project(appName, appVersion, appDependencies).settings(
-    lessEntryPoints <<= baseDirectory(customLessEntryPoints)
+    lessEntryPoints <<= baseDirectory(customLessEntryPoints),
+    routesImport += "se.radley.plugin.salat.Binders._",
+    templatesImport += "org.bson.types.ObjectId",
+    resolvers += "Maven Central" at "http://repo1.maven.org/maven2",
+    resolvers += "Typesafe Repository 2" at "http://repo.typesafe.com/typesafe/repo/"
   )
   
 }
