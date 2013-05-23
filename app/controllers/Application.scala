fd258c01b8d6ba152f826047b333079171516c85
Umut Benzer <ub@ubenzer.com>
2013-05-23 22:37:48 +0300

    Application.java scalafied.

---
+++ app/controllers/Application.scala
@@ -0,0 +1,20 @@
+package controllers
+
+import play.api.mvc._
+import play.api.mvc.Controller
+import play.i18n.Lang
+
+object Application extends Controller {
+  def index = Action {
+    Ok(views.html.index.render())
+  }
+
+  def javascriptRoutes = Action { implicit request =>
+    import routes.javascript._
+    Ok(play.api.Routes.javascriptRouter("routes")(
+      Swoogle.submit,
+      Watson.submit,
+      Sindice.submit)
+    ).as("text/javascript")
+  }
+}
