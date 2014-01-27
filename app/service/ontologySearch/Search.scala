cffcbd8520c916d41ce48dc6c9719eaf5bcb3421
Umut Benzer <ub@ubenzer.com>
2014-01-27 21:36:16 +0200

    Experiments on reading mongodb results.

--- app/service/ontologySearch/Search.scala
+++ app/service/ontologySearch/Search.scala
@@ -5,13 +5,16 @@ import com.mongodb.casbah.Imports._
 
 object Search {
 
+  val specialElements =
+   "http://www.w3.org/2000/01/rdf-schema#comment" :: Nil
+
   def findElementsByKeyword(kw: String) = {
-    val cr1 = OntologyElement.collection.db.command(DBObject("text" -> "OntologyElement") ++ DBObject("search" -> kw, "filter" -> DBObject("isBlankNode" -> false)))
-    val cr2 = OntologyTriple.collection.db.command(DBObject("text" -> "OntologyTriple") ++ DBObject("search" -> kw))
+    val cr1: DBObject = OntologyElement.collection.db.command(DBObject("text" -> "OntologyElement") ++ DBObject("search" -> kw, "filter" -> DBObject("isBlankNode" -> false)))
+    val cr2: DBObject = OntologyTriple.collection.db.command(DBObject("text" -> "OntologyTriple") ++ DBObject("search" -> kw))
 
-    cr1.result()
+    val Z = cr2.get("results").asInstanceOf[DBObject]
 
-    (cr1, cr2)
+    (Z.size, cr2)
   }
   def findSubject(predicate: String, objekt: String) = {}
   def findPredicate(predicate: String, objekt: String) = {}
