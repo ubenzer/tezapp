9414e66e690de0b3af4c9ddb294271e45ce7f837
Umut Benzer <ub@ubenzer.com>
2014-02-02 15:23:39 +0200

    Some progres on keyword search algorithm

--- app/service/ontologySearch/Search.scala
+++ app/service/ontologySearch/Search.scala
@@ -6,14 +6,39 @@ import common.ExecutionContexts.fastOps
 import reactivemongo.bson.{BSONArray, BSONDocument}
 import reactivemongo.core.commands.RawCommand
 import scala.util.{Success, Failure}
+import java.util.Locale
 
 object Search {
 
-  val specialElements =
-   "http://www.w3.org/2000/01/rdf-schema#label"   ::
-   "http://www.w3.org/2000/01/rdf-schema#comment" :: Nil
+  sealed trait DescriptiveElement // http://stackoverflow.com/a/18595574/158523
+  object Label extends DescriptiveElement
+  object Comment extends DescriptiveElement
 
   def findElementsByKeyword(kws: String*): Future[Seq[SearchResult]] = {
+    import scala.annotation.tailrec
+    def countSubstring(str1:String, str2:String):Int= {    // http://rosettacode.org/wiki/Count_occurrences_of_a_substring#Scala
+      @tailrec def count(pos:Int, c:Int):Int={
+        val idx=str1 indexOf(str2, pos)
+        if(idx == -1) c else count(idx+str2.size, c+1)
+      }
+      count(0,0)
+    }
+    def getMatchCount(keywords: Seq[String], tbSearchedIn: String): Int = {
+      keywords.foldLeft(0) {
+        (aggr, kw) => aggr + countSubstring(tbSearchedIn.toLowerCase(Locale.ENGLISH), kw)
+      }
+    }
+    def getAsDescriptiveElement(uri: String): Option[DescriptiveElement] = {
+
+      val descriptiveElementLookup: Map[String, DescriptiveElement] = Map(
+        "http://www.w3.org/2000/01/rdf-schema#label" -> Label,
+        "http://www.w3.org/2000/01/rdf-schema#comment" -> Comment
+      )
+
+      descriptiveElementLookup.get(uri)
+    }
+
+
     val searchCommand = BSONDocument(
       "text" -> OntologyTriple.collection.name,
       "search" -> kws.mkString(" "),
@@ -21,29 +46,64 @@ object Search {
     )
     val futureResult: Future[BSONDocument] = OntologyTriple.collection.db.command(RawCommand(searchCommand))
 
-
     futureResult.map {
       bson =>
-        println(BSONDocument.pretty(bson))
-        val results = bson.getAs[BSONArray]("results").get
+        val results = bson.getAs[BSONArray]("results").getOrElse(BSONArray.empty)
         results.iterator.map {
           case Failure(ex) => None
           case Success((idx, value: BSONDocument)) => {
             val score = value.getAs[Double]("score").get
             val triple = value.getAs[OntologyTriple]("obj").get
 
-            if(specialElements.contains(triple.predicate)) {
-              println("Özel durum!")
+            /* Determine what matched keywords */
+            val objektHitCount = getMatchCount(kws, triple.objekt)
+            val predicateHitCount = getMatchCount(kws, triple.predicate)
+            val subjectHitCount = getMatchCount(kws, triple.subject)
 
-              Some(
-                SearchResult(triple.subject, Some(triple.id.get.toString), None, "AHAHA", score)
-              )
-            } else {
-              println("Değil")
-              println(value)
-              None
+            val realHitElement = (objektHitCount :: predicateHitCount :: subjectHitCount :: Nil).view.sorted.reverse.head match {
+              case `objektHitCount` => triple.objekt
+              case `predicateHitCount` => triple.predicate
+              case `subjectHitCount` => triple.subject
             }
+
+            val sr = realHitElement match {
+              case triple.objekt => {
+                val sr = SearchResult(
+                    uri = triple.objekt,
+                    kind = "TODO",
+                    score = score
+                )
+
+                getAsDescriptiveElement(triple.predicate).map {
+                  case Label => sr.copy(
+                    uri   = triple.subject,
+                    label = Some(triple.objekt)
+                  )
+
+                  case Comment => sr.copy(
+                    uri     = triple.subject,
+                    comment = Some(triple.objekt)
+                  )
+                  case _ => sr
+                }.getOrElse {
+                  println("UMUT 1000 TODO")
+                  sr
+                }
+              }
+              case triple.predicate => SearchResult(
+                uri = triple.predicate,
+                kind = "TODO",
+                score = score
+              )
+              case triple.subject => SearchResult(
+                uri = triple.subject,
+                kind = "TODO",
+                score = score
+              )
+            }
+            Some(sr)
           }
+          case Success(_) => None
         }.foldLeft(List.empty[SearchResult]) {
           case (previous, Some(searchResult)) => searchResult :: previous
           case (previous, None) => previous
