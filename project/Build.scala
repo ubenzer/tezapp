da91f999fe7d8dc88e0bee6a2769a7839c411c17
Umut Benzer <ub@ubenzer.com>
2014-02-15 17:19:44 +0200

    Upgraded rio to 2.7.10

--- project/Build.scala
+++ project/Build.scala
@@ -8,9 +8,22 @@ object ApplicationBuild extends Build {
   val appVersion      = "1.0-SNAPSHOT"
 
   val appDependencies = Seq(
-    // Add your project dependencies here,
-    "org.openrdf.sesame" % "sesame-runtime" % "2.7.9",
-    "org.reactivemongo" %% "play2-reactivemongo" % "0.10.2"
+    "commons-codec" % "commons-codec" % "1.9",
+    "org.reactivemongo" %% "play2-reactivemongo" % "0.10.2",
+
+    "org.openrdf.sesame" % "sesame-rio"           % "2.7.10",
+    "org.openrdf.sesame" % "sesame-rio-api"       % "2.7.10",
+    "org.openrdf.sesame" % "sesame-rio-trig"      % "2.7.10",
+    "org.openrdf.sesame" % "sesame-rio-trix"      % "2.7.10",
+    "org.openrdf.sesame" % "sesame-rio-rdfxml"    % "2.7.10",
+    "org.openrdf.sesame" % "sesame-rio-rdfjson"   % "2.7.10",
+    "org.openrdf.sesame" % "sesame-rio-nquads"    % "2.7.10",
+    "org.openrdf.sesame" % "sesame-rio-ntriples"  % "2.7.10",
+    "org.openrdf.sesame" % "sesame-rio-n3"        % "2.7.10",
+    "org.openrdf.sesame" % "sesame-rio-turtle"    % "2.7.10",
+    "org.openrdf.sesame" % "sesame-rio-binary"    % "2.7.10",
+    "org.openrdf.sesame" % "sesame-rio-datatypes" % "2.7.10",
+    "org.openrdf.sesame" % "sesame-rio-languages" % "2.7.10"
   )
 
   // Only compile the bootstrap bootstrap.less file and any other *.less file in the stylesheets directory 
