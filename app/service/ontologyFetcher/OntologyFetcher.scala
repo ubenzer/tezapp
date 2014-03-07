399b02786a8419acb8adbbf82bd7945d3673590d
Umut Benzer <ub@ubenzer.com>
2014-03-07 23:01:31 +0200

    Better memory management

--- app/service/ontologyFetcher/OntologyFetcher.scala
+++ app/service/ontologyFetcher/OntologyFetcher.scala
@@ -1,6 +1,6 @@
 package service.ontologyFetcher
 
-import scala.concurrent.Future
+import scala.concurrent.{ExecutionContext, Future}
 import play.api.libs.ws.{WS, Response}
 import play.api.Logger
 import java.net.ConnectException
@@ -14,40 +14,53 @@ import service.ontologyFetcher.storer.MongoStorageEngine
 abstract class OntologyFetcher(parser: OntologyParser) {
   def getOntologyList(keyword: String): Future[Set[String]]
 
-  protected def search(keyword: String, source: String): Future[FetchResult] = {
-    Logger.info("Starting search for keyword '" + keyword + "'")
+  def serialiseFutures[A, B](l: Iterable[A])(fn: A ⇒ Future[B])
+                            (implicit ec: ExecutionContext): Future[List[B]] =
+    l.foldLeft(Future(List.empty[B])) {
+      (previousFuture, next) ⇒
+        for {
+          previousResults ← previousFuture
+          next ← fn(next)
+        } yield previousResults :+ next
+    }
 
+  protected def search(keyword: String, source: String): Future[FetchResult] = {
+    import ExecutionContexts.fastOps
+    Logger.info("Starting search for keyword '" + keyword + "'")
     /* Step 1: Fetch ontology list to be fetched. */
     val ontologyListF = getOntologyList(keyword)
     val cleanedOntologyList = ontologyListF.map {
-      ontologyList => ontologyList.filter(x => x.startsWith("http://") || x.startsWith("https://"))
-    }(ExecutionContexts.fastOps)
+      ontologyList => ontologyList.filter(x => x.startsWith("http://") || x.startsWith("https://")).toList
+    }
 
     cleanedOntologyList.flatMap {
       case ontologyList if ontologyList.isEmpty => Future.successful(FetchResult(searchEngineFailed = true))
       case ontologyList if !ontologyList.isEmpty =>
         Logger.info("Downloading ontologies, total file count: " + ontologyList.size)
-        val downloadedOntologiesFutureSeq: Seq[Future[(Option[Response], FetchResult)]] = downloadOntologies(ontologyList.toSeq: _*)
-        val processedOntologiesSeq: Seq[Future[FetchResult]] = downloadedOntologiesFutureSeq.map {
-          downloadedOntologiesFuture =>
-            downloadedOntologiesFuture.flatMap {
-              case (Some(ontologyResponse), fetchResult) =>
-                val fetchResultF = parser.parseResponseAsOntology(ontologyResponse, source)
-                fetchResultF
-              case (None, fetchResult) => Future.successful(fetchResult)
-            }(ExecutionContexts.verySlowOps)
-        }
-        import ExecutionContexts.fastOps
-        Future.sequence(processedOntologiesSeq).map {
+
+        serialiseFutures(ontologyList.grouped(100).toIterable) {
+          aChunk =>
+            def downloadedOntologiesFutureSeq: List[Future[(Option[Response], FetchResult)]] = downloadOntologies(aChunk)
+            def processedOntologiesSeq: List[Future[FetchResult]] = downloadedOntologiesFutureSeq.map {
+              downloadedOntologiesFuture =>
+                downloadedOntologiesFuture.flatMap {
+                  case (Some(ontologyResponse), fetchResult) =>
+                    val fetchResultF = parser.parseResponseAsOntology(ontologyResponse, source)
+                    fetchResultF
+                  case (None, fetchResult) => Future.successful(fetchResult)
+                }
+            }
+            Future.sequence(processedOntologiesSeq)
+        }.map {
           fr =>
-            fr.foldLeft(FetchResult()) {
+            fr.flatten.foldLeft(FetchResult()) {
               (result, current) => result + current
-          }
+            }
         }
-    }(ExecutionContexts.internetIOOps)
+    }
   }
 
-  def downloadOntologies(urlList: String*): Seq[Future[(Option[Response], FetchResult)]] = {
+  def downloadOntologies(urlList: List[String]): List[Future[(Option[Response], FetchResult)]] = {
     import ExecutionContexts.internetIOOps
     val resultFutures = urlList.map {
       url =>
